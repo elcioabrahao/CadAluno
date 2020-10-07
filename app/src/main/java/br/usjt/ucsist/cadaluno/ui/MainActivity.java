53eb73df4d5700d27e5c4b222386298a7bf9a4b2
2104499700 <elcio.abrahao@viavarejo.com.br>
2020-10-06 21:47:30 -0300

    Implementação do nav bar

--- app/src/main/java/br/usjt/ucsist/cadaluno/ui/MainActivity.java
+++ app/src/main/java/br/usjt/ucsist/cadaluno/ui/MainActivity.java
@@ -1,20 +1,78 @@
 package br.usjt.ucsist.cadaluno.ui;
 
+import androidx.annotation.IdRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import br.usjt.ucsist.cadaluno.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        replaceFragment(R.id.frameLayout,
+                HomeFragment.newInstance("",""),
+                "HOMEFRAGMENT",
+                "HOME");
+
+
+        bottomNav = findViewById(R.id.bottomNav);
+
+        bottomNav.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+
+                    case R.id.home:
+                        replaceFragment(R.id.frameLayout,
+                                HomeFragment.newInstance("",""),
+                                "HOMEFRAGMENT",
+                                "HOME");
+                        return true;
+
+                    case R.id.contatos:
+                        replaceFragment(R.id.frameLayout,
+                                ContatoFragment.newInstance("",""),
+                                "CONTATOFRAGMENT",
+                                "CONTATO");
+                        return true;
+
+                    case R.id.perfil:
+                        replaceFragment(R.id.frameLayout,
+                                PerfilFragment.newInstance("",""),
+                                "PERFILFRAGMENT",
+                                "PERFIL");
+                        return true;
+
+                    case R.id.configuracao:
+                        replaceFragment(R.id.frameLayout,
+                                ConfiguracaoFragment.newInstance("",""),
+                                "CONFIGURACAOFRAGMENT",
+                                "CONFIGURACAO");
+                        return true;
+
+
+                }
+                return false;
+            }
+                });
+
     }
 
     @Override
@@ -35,5 +93,16 @@ public class MainActivity extends AppCompatActivity {
                 return(true);
         }
         return(super.onOptionsItemSelected(item));
+    }
+
+    protected void replaceFragment(@IdRes int containerViewId,
+                                   @NonNull Fragment fragment,
+                                   @NonNull String fragmentTag,
+                                   @Nullable String backStackStateName) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(containerViewId, fragment, fragmentTag)
+                .addToBackStack(backStackStateName)
+                .commit();
     }
 }
